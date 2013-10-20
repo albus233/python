3703b366c67f6ec2a0359f6ae8b499eddbe88bd2
albus233 <pp19980427@126.com>
2013-10-20 21:03:21 +0800

    the main part of the blog

--- flaskr.py
+++ flaskr.py
@@ -4,10 +4,9 @@ import sqlite3
 from sqlite3 import *
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 from sqlalchemy import *
-from datetime import *
+from datetime import datetime
 from sqlalchemy.orm import *
-from flask.ext.superadmin import *
-from flask.ext.sqlalchemy import *
+from flask.ext.sqlalchemy import SQLAlchemy
 
 from app import app, db, User, entries, create
 
@@ -20,18 +19,21 @@ app.config.from_object(__name__)
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 create()
+user = 'None'
 
 @app.route('/')
 def show_entries():
+#    entriess = entries.query.all().order_by(entries.id.desc())[:8]
     cur = db.session.execute('select title, text from entries order by id desc')
     entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
     return render_template('show_entries.html', entries=entries)
 
 @app.route('/add', methods=['POST'])
 def add_entry():
+    global ans
     if not session.get('logged_in'):
         abort(401)
-    t = entries(request.form['title'], request.form['text'])
+    t = entries(request.form['title'], request.form['text'], user)
     db.session.add(t)
     db.session.commit()
     flash('New entry was successfully posted')
@@ -39,28 +41,73 @@ def add_entry():
 
     
 @app.route('/login', methods=['GET', 'POST'])
-def login():
+def login():  
     error = None
-    if request.method == 'POST':    
+    if request.method == 'POST':
         username = request.form["username"]
-        password = request.form["password"]  
+        password = request.form["password"]
         u = User.query.filter_by(username=username).first_or_404()
         if u.password == password:
             session['logged_in'] = True
-            flash('You were logged in')
+            global user
+            user = username
+            flash('You were logged in!')
+            flash(user)
             return redirect(url_for('show_entries'))
+        else:
+            flash('Wrong password')
+            return redirect(url_for('login'))
     return render_template('login.html', error=error)
 
 @app.route('/logout')
 def logout():
+    global user
     session.pop('logged_in', None)
+    user = 'None'
     flash('You were logged out')
+    return redirect(url_for('show_entries'))
+
+@app.route('/delete', methods = ['POST'])
+def delete():
+    tit = request.form["delete"]
+    owner = entries.query.filter_by(title = tit).first()
+    if user == 'admin':
+        db.session.delete(owner)
+        db.session.commit()
+    elif user == owner.publisher:
+        db.session.delete(owner)
+        db.session.commit()
+    else:
+        flash('You don\' have permission for this')
     return redirect(url_for('show_entries'))
 
 @app.route('/rr')
 def rr():
     error = None
     return render_template('register.html', error=error)
+
+@app.route('/r')
+def r():
+    error = None
+    return render_template('repwd.html', error=error)
+
+@app.route('/repwd', methods=['POST'])
+def repwd():
+    global user
+    if not session.get('logged_in'):
+        abort(401)
+    else:
+        password = request.form["password"]
+        u = User.query.filter_by(username = user).first_or_404()
+        db.session.delete(u)
+        db.session.commit()
+        u = User(user, password)
+        db.session.add(u)
+        db.session.commit()
+        session.pop('logged_in', None)
+        user = 'None'
+        flash('Success!')
+        return redirect(url_for('login'))
 
 @app.route('/register', methods=['GET','POST'])
 def register():
@@ -70,11 +117,16 @@ def register():
         password = request.form['password']
         repassword = request.form['repeatpassword']
         if repassword == password:
-            t1 = User(username, password)
-            db.session.add(t1)
-            db.session.commit()
-            flash('Register successfully!')
-            return redirect(url_for('login'))
+            u = User.query.filter_by(username = username).first()
+            if u == None:
+                t1 = User(username, password)
+                db.session.add(t1)
+                db.session.commit()
+                flash('Register successfully!')
+                return redirect(url_for('login'))
+            else:
+                flash('The username has been registed')
+                return redirect(url_for('rr'))
         else:
             flash('password is not same')
             return redirect(url_for('rr'))

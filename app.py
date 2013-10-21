e9b4c28e46a0b5d974e73e8cd41020885259c204
albus233 <pp19980427@126.com>
2013-10-21 23:00:39 +0800

    i have change the style of class

--- app.py
+++ app.py
@@ -1,28 +1,29 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.superadmin import Admin
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flaskr.db'
 db = SQLAlchemy(app)
 
+
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(50), nullable = False)
-    password = db.Column(db.String(50), nullable = False)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(50), nullable=False)
 
     def __init__(self, username, password):
         self.username = username
         self.password = password
 
     def __repr__(self):
-        return '<User %r>' %self.username
+        return '<User %r>' % self.username
 
-class entries(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    title = db.Column(db.String(120), nullable = False)
-    text = db.Column(db.Text, nullable = False)
-    publisher = db.Column(db.String(50), nullable = False)
+
+class Entries(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    publisher = db.Column(db.String(50), nullable=False)
 
     def __init__(self, title, text, publisher):
         self.title = title
@@ -30,11 +31,12 @@ class entries(db.Model):
         self.publisher = publisher
 
     def __repr__(self):
-        return '<entries %r>' %self.title
+        return '<entries %r>' % self.title
+
 
 def create():
     db.create_all()
-    if User.query.filter_by(username='admin').first() == None:
+    if User.query.filter_by(username='admin').first() is None:
         admin = User('admin', 'albus233')
         db.session.add(admin)
     db.session.commit()
